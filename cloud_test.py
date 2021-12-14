326dd0dd0b15af85c890aba071a9896ce8822761
yusuf <yusuf@thinkpad>
2021-12-14 15:03:53 +0300

    cloud

---
+++ cloud_test.py
@@ -0,0 +1,22 @@
+#%%
+import requests
+
+
+url = 'https://ml-capstone.azurewebsites.net/predict'
+
+
+
+
+test = {
+	"vehicle_class": "subcompact",
+	"engine_size": 1.5,
+	"cylinders": 3,
+	"fuel_type": "z",
+	"fuel_consumption_city":8.3,
+	"fuel_consumption_hwy":6.4,
+	"fuel_consumption_comb_mpg":38
+
+
+}
+response = requests.post(url, json=test).json()
+print(response)
